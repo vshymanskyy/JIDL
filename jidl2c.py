6e21dd65bdd71a744883c5b4fc42890a473ce594
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-04-07 14:54:32 +0300

    More relaxed items

--- jidl2c.py
+++ jidl2c.py
@@ -74,6 +74,12 @@ def normalize_idl(idl):
     if "@output_dir" not in idl:
         idl["@output_dir"] = "./output"
 
+    if "types" not in idl:
+        idl["types"] = {}
+
+    if "interfaces" not in idl:
+        idl["interfaces"] = {}
+
     for name, data in idl["types"].items():
         if isinstance(data, str):
             idl["types"][name] = { "type": "alias", "for": data }
